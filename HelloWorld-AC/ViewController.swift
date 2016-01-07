2bd315f63f8fc0ae21fd3c597aa9fc43fcee3106
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-07 15:21:24 -0500

    App Coda Chapter 3

---
+++ HelloWorld-AC/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloWorld-AC
+//
+//  Created by Xiulan Shi on 1/7/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
+
